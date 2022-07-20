7a5774b17ee06aaf2cc9e852d67ac2d4f0c35b2d
Kong Kyu <gyu2739@gmail.com>
2022-07-21 03:33:11 +0900

    Fix goose grab
    가벼운 오브젝트 잡는거 성공

--- Assets/AATeamProject/Scripts/Goose/GooseGrab.cs
+++ Assets/AATeamProject/Scripts/Goose/GooseGrab.cs
@@ -23,47 +23,63 @@ public class GooseGrab : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            if (isDrag)
+            switch(isDrag)
             {
-                isDrag = false;
-                grabObject = null;
+                case true:
+                    isDrag = false;
+
+                    //grabObject의 rigidBody를 원래 상태로 되돌려야함.
+                    //grabObject를 자식오브젝트로 배치한거 풀어야함.
+
+                    grabObject.transform.SetParent(null);
+
+                    grabObject = null;  
+                    break;
+                case false:
+                    if (grabObject != null) //
+                    {
+                        isDrag = true;
+                        Rigidbody grabObjRb = grabObject.GetComponent<Rigidbody>();
+
+                        if (!grabObject.GetComponent<PhysicObject>().isHeavy)   //가벼운 오브젝트 잡을 때
+                        {
+                            Debug.Log("Success Grab");
+
+                            var rot = gooseMouse.transform.eulerAngles - grabObject.GetComponent<SmallObject>().handlePoint.transform.eulerAngles;
+
+                            grabObjRb.transform.eulerAngles += rot;
+
+                            var pos = gooseMouse.position - grabObject.GetComponent<SmallObject>().handlePoint.transform.position;
+                            grabObject.transform.position += pos;
+
+                            grabObjRb.useGravity = false;
+                            grabObjRb.isKinematic = false;
+                            grabObjRb.mass = 0f;
+                            grabObjRb.constraints = RigidbodyConstraints.FreezeAll;
+                            grabObject.transform.SetParent(gooseMouse);
+                        }
+                        else if(grabObject.GetComponent<PhysicObject>().isHeavy)    //무거운 오브젝트 잡을 때
+                        {
+                            
+                            var distances = grabObject.GetComponent<BigObject>().handlePoint;
+                            foreach(var distance in distances)
+                            {
+                                //여기서 거리 짧은애 구함
+                            }
+                            //그다음 여기서 위에 있는 코드들이랑 똑같이 작성하면 끝?
+                            //단 inverse kinematic 애니메이션을 사용하여 부리가 handlePoint에 닿게 해야함.
+                            
+                        }
+                    }
+                    break;
             }
 
-            if (grabObject != null)
-            {
-                isDrag = true;
-                Rigidbody grabObjRb = grabObject.GetComponent<Rigidbody>();
-                if (!grabObject.GetComponent<PhysicObject>().isHeavy)
-                {
-                    Debug.Log("Success Grab");
-                    
-                    var pos = gooseMouse.position - grabObject.GetComponent<SmallObject>().handlePoint.transform.position;
-                    grabObject.transform.position += pos;
-                    //grabObject.transform.rotation = gooseMouse.transform.rotation;//grabObject.GetComponent<SmallObject>().handlePoint.transform.rotation;
-                    var rot = gooseMouse.transform.eulerAngles - grabObject.GetComponent<SmallObject>().handlePoint.transform.eulerAngles;
-                    
-                    grabObjRb.transform.eulerAngles += rot;
+           
 
-                    grabObjRb.useGravity = false;
-                    grabObjRb.isKinematic = false;
-                    grabObjRb.mass = 0f;
-                    grabObjRb.constraints = RigidbodyConstraints.FreezeAll;
-                    grabObject.transform.SetParent(gooseMouse);
-                    
-                   // var fixedJoint = grabObject.AddComponent<FixedJoint>();
-                     //fixedJoint.connectedBody = rb;
-                    // fixedJoint.connectedAnchor = gameObject.GetComponent<SmallObject>().handlePoint.transform.position;
-                }
-                
-
-                // gooseMouse.transform.position = grabObject.GetComponent<TestGrabHandle>().grabHandle.transform.localPosition;
-
-                //grabObject.transform.SetParent(gooseMouse);
-            }
+           
         }
        
 
-        //collider.transform.position = gooseMouse.transform.position;
     }
 
 
