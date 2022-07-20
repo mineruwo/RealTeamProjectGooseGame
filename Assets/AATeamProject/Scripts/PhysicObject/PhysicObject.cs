4cec0d19368964e37bc9d1ca4317168e5272f2e2
Son_Minwoo <99243341+mineruwo@users.noreply.github.com>
2022-07-20 15:28:57 +0900

    modify - Cam cart Setting

---
+++ Assets/AATeamProject/Scripts/PhysicObject/PhysicObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PhysicObject : MonoBehaviour
+{
+
+    public bool isGrab;
+
+    public bool isSound;
+
+    public bool isHeavy;
+
+    public bool isActive;
+
+    
+    public abstract bool OnTrigger();
+
+    public abstract bool OnGrab(bool isgrab);
+
+}
