f30f070573968ab44e754b26609f49b947963507
Son_Minwoo <99243341+mineruwo@users.noreply.github.com>
2022-07-22 15:47:46 +0900

    [2022-07-22] modify openning skip

--- Assets/AATeamProject/Scenes/MW/OpenningScript.cs
+++ Assets/AATeamProject/Scenes/MW/OpenningScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class OpenningScript : MonoBehaviour
 {
+    public bool isSkip = false;
+    public void Update()
+    {
+        if (Input.anyKeyDown && !isSkip)
+        {
+            NextScene();
+            isSkip = true;
+        }
+    }
     public void NextScene()
     {
         GameManager.instance.sceneMgr.NextScene();
